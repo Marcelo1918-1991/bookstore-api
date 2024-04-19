c80771a4ab9259c7ec6aa092baca3386e601a1fd
Marcelo1918-1991 <marcelo.vinicius99@outlook.com>
2024-04-18 23:02:12 -0300

    comitando endpoint para pegar todas as categorias

--- src/main/java/com/example/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/example/bookstore/resources/CategoriaResource.java
@@ -1,5 +1,9 @@
 package com.example.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bookstore.domain.Categoria;
+import com.example.bookstore.dtos.CategoriaDTO;
 import com.example.bookstore.service.CategoriaService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -20,7 +25,17 @@ public class CategoriaResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id) throws ObjectNotFoundException {
-		Categoria categoria = categoriaService.findById(id);
+		Categoria categoria = categoriaService.pegarPorId(id);
 		return ResponseEntity.ok().body(categoria);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = categoriaService.pegarTodasCategorias();
+		List<CategoriaDTO> listDTO = list.stream().map(
+				obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+
+		
 	}
 }
--- src/main/java/com/example/bookstore/service/CategoriaService.java
+++ src/main/java/com/example/bookstore/service/CategoriaService.java
@@ -1,5 +1,8 @@
 package com.example.bookstore.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,18 +11,20 @@ import com.example.bookstore.repositories.CategoriaRepository;
 
 import javassist.tools.rmi.ObjectNotFoundException;
 
-import java.util.Optional;
-
 @Service
 public class CategoriaService {
 
 	@Autowired
-	private CategoriaRepository categoriaRepository; 
-	
-	public Categoria findById(Integer id) throws ObjectNotFoundException {
-	    Optional<Categoria> categoria = categoriaRepository.findById(id);
-	    return categoria.orElseThrow(() -> new ObjectNotFoundException(
-	                    "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	private CategoriaRepository categoriaRepository;
+
+	public Categoria pegarPorId(Integer id) throws ObjectNotFoundException {
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public List<Categoria> pegarTodasCategorias() {
+		return categoriaRepository.findAll();
 	}
 
 }
